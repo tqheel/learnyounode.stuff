1a57b4072ccb9e5940accf734454e51dec2d0653
Todd Qualls <tqualls@gmail.com>
2015-02-06 22:55:56 -0500

    Completes http client lesson.

---
+++ httpclient.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+var url = process.argv[2];
+
+http.get(url, function(response){
+	response.setEncoding('utf8');
+	response.on('data', function(data){
+		console.log(data);
+	});
+	response.on('error', function(error){
+		error.forEach(function(e){
+			console.log(e);
+		});
+	});	
+	response.on('end', function(){
+		
+	});
+});
