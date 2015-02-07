9e9e4cd0fe5bd3ed48bfcf3207ef4c5aee4eb5a5
Todd Qualls <tqualls@gmail.com>
2015-02-07 16:41:49 -0500

    Creates primitive message client and server.

--- msgclient.js
+++ msgclient.js
@@ -2,15 +2,17 @@ var net = require('net');
 
 var client = new net.Socket();
 var host = '127.0.0.1';
-var port = 6767;
+var port = 1337;
 
 
 client.connect(port, host, function(){
 	console.log('Connected to: ' +host+':'+port);
+	var msg = process.argv[2];
+	client.write(msg);
 });
 
 client.on('data', function(data){
-	console.log('The time is: ' + data);
+	console.log(data.toString());
 	client.destroy();
 });
 client.on('close', function() {
---
+++ msgserver.js
@@ -0,0 +1,31 @@
+var net = require('net');
+
+function createTimeString(){
+	date = new Date();
+	var y = date.getFullYear();
+	var m = zeroPadIfNeeded(date.getMonth()+1);
+	var d = zeroPadIfNeeded(date.getDate());
+	var h = zeroPadIfNeeded(date.getHours());
+	var min = zeroPadIfNeeded(date.getMinutes());
+	var dash = '-';
+	return y+dash+m+dash+d+' '+h+':'+min;
+
+}
+function zeroPadIfNeeded(input){
+	return (input<10) ? '0' + input.toString() : input.toString();
+}
+
+var server = net.createServer(function(socket){
+	socket.on('data', function(data){
+		console.log('Received message: '+data + ' from '+socket.remoteAddress);
+		var time = createTimeString();
+		socket.write('At ' + time + ', you said, "'+data+'"');
+	});
+	
+});
+var port = 1337;
+var host = '127.0.0.1';
+server.listen(port,host);
+
+console.log('Server listening on '+ host+':'+port);
+
